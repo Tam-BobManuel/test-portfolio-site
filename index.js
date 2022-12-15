272445f177ca0e75a981607bb2f9f987fefc0184
unknown <bglorytb@gmail.com>
2022-12-15 22:33:25 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log(`hey you peasky developer, let's work together`);
+          
+          // Nav bar
+
+function burger(){
+    document.getElementById("BarDropDiv-Container").classList.toggle("active");
+}
